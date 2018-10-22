38ef6abb9b1d499b9ad7b7a7e3fc0e40d2e30e62
Jean Leão <JCLSoftware@users.noreply.github.com>
2018-10-22 15:51:31 -0300

    Update LoadPNAD.R

--- src/LoadPNAD.R
+++ src/LoadPNAD.R
@@ -1,6 +1,5 @@
-#LEITURA DA PNAD 2018: rm(list=ls(all=TRUE))
-#make sure to set correctly your working directory 
-setwd("~/Projetcs/R/PNADRegression/")
+#LEITURA DA PNAD 2018: 
+#make sure to set correctly your working directory Ex: setwd("~/Projetcs/R/PNADRegression/")
 # mostrar até 8 casas decimais options("scipen" = 8) 
 #Arquivo de metadados (propriedades dos campos de dados)
 source<-'https://raw.githubusercontent.com/JCLSoftware/PNADRegression/master/data/meta.csv'
@@ -16,7 +15,8 @@ target<-'meta.txt'
 writeLines(tx2, con=target)
 config<-read.csv(file=target, sep = ' ', header = F)
 #Arquivo de dados
-sourceData<-'data/PNADC_012017_20180816.zip'
+#sourceData<-'data/PNADC_012017_20180816.zip'
+csvFile = gsub(pattern = '.txt$', replace = '.csv$', x = sourceData)
 if(!file.exists(sourceData)){
   pes2018 <- read.fwf(file='data/PNADC_012017_20180816/PNADC_012017_20180816.txt', widths=config$V3)
   names(pes2018) <-config$V2
@@ -30,10 +30,12 @@ if(!file.exists(sourceData)){
   #V2009: Idade
   #VD3002: Anos de estudo
   selectedCols <- subset(pes2018, select=c("UF","V2007","V2009","V2010","VD3002","VD4020","V1023","V1022","V1028"))
-  sourceDataCSV<-paste(sourceData, '.csv', sep='')
-  write.csv(pes2018b, file=sourceDataCSV, row.names = F)
-  #Zip file PNADC_012017_20180816.csv -> PNADC_012017_20180816.zip
-  zip(zipfile = sourceData, files = 'data/PNADC_012017_20180816.csv')
+  write.csv(pes2018b, file=csvFile, row.names = F)
+  #Zip file PNADC_012017_20180816.csv -> PNADC_012017_20180816.zip  
+  zipFile <- gsub(pattern = '.csv$', replace = '.zip$', x = sourceData)
+  if(!file.exists(zipFile)){
+    zip(zipfile = zipFile, files = csvFile)
+  }
 }
 
-selectedCols<-read.csv(file = unz(sourceData, "PNADC_012017_20180816.csv"))
+selectedCols<-read.csv(file = unz(sourceData, csvFile))
