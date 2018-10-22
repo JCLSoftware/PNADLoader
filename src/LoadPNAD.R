f02730a5893ff597de5e8a840ff7d42a3041b7fb
Jean Leão <JCLSoftware@users.noreply.github.com>
2018-10-22 16:22:16 -0300

    Update LoadPNAD.R

--- src/LoadPNAD.R
+++ src/LoadPNAD.R
@@ -3,22 +3,29 @@
 # mostrar até 8 casas decimais options("scipen" = 8) 
 #Arquivo de metadados (propriedades dos campos de dados)
 #sourceMeta<-'https://raw.githubusercontent.com/JCLSoftware/PNADRegression/master/data/meta.csv'
-tx  <- readLines(sourceMeta)
-tx2  <- gsub(pattern = " $", replace = " ", x = tx, fixed=T)
-tx2  <- gsub(pattern = "  ", replace = " ", x = tx2)
-tx2  <- gsub(pattern = "  ", replace = " ", x = tx2)
-tx2  <- gsub(pattern = "  ", replace = " ", x = tx2)
-tx2  <- gsub(pattern = ". ", replace = " ", x = tx2, fixed=T)
-tx2
 #target<-paste(source, '.txt', sep='')
 target<-'meta.txt'
-writeLines(tx2, con=target)
-config<-read.csv(file=target, sep = ' ', header = F)
+
+buildMeta<-function(src){
+  tx  <- readLines(src)
+  tx2  <- gsub(pattern = " $", replace = " ", x = tx, fixed=T)
+  tx2  <- gsub(pattern = "  ", replace = " ", x = tx2)
+  tx2  <- gsub(pattern = "  ", replace = " ", x = tx2)
+  tx2  <- gsub(pattern = "  ", replace = " ", x = tx2)
+  tx2  <- gsub(pattern = ". ", replace = " ", x = tx2, fixed=T)
+  tx2
+  writeLines(tx2, con=target)
+}
+readMeta<-function(){
+  read.csv(file=target, sep = ' ', header = F)
+}
+buildMeta(sourceMeta)
+config<-readMeta()
 #Arquivo de dados
 #sourceData<-'data/PNADC_012017_20180816.zip'
-csvFile = gsub(pattern = '.zip$', replace = '.csv$', x = sourceData)
+csvFile = gsub(pattern = '.zip$', replace = '.csv', x = sourceData)
 if(!file.exists(sourceData)){
-  pes2018 <- read.fwf(file='data/PNADC_012017_20180816/PNADC_012017_20180816.txt', widths=config$V3)
+  pes2018 <- read.fwf(file=gsub(pattern = '.zip$', replace = '.txt', x = sourceData), widths=config$V3)
   names(pes2018) <-config$V2
   #View(pes2018)
   summary(pes2018) 
@@ -29,13 +36,17 @@ if(!file.exists(sourceData)){
   #V2007: Sexo
   #V2009: Idade
   #VD3002: Anos de estudo
-  selectedCols <- subset(pes2018, select=c("UF","V2007","V2009","V2010","VD3002","VD4020","V1023","V1022","V1028"))
+  selectedCols <- subset(pes2018, select=selectedFields)
   write.csv(pes2018b, file=csvFile, row.names = F)
   #Zip file PNADC_012017_20180816.csv -> PNADC_012017_20180816.zip  
-  zipFile <- gsub(pattern = '.csv$', replace = '.zip$', x = sourceData)
-  if(!file.exists(zipFile)){
-    zip(zipfile = zipFile, files = csvFile)
+  if(!file.exists(sourceData)){
+    zip(zipfile = sourceData, files = csvFile)
   }
 }
+if(file.exists(sourceData)){
+  f<-unz(sourceData, csvFile)
+}else{
+  f<-csvFile
+}
 
-selectedCols<-read.csv(file = unz(sourceData, csvFile))
+selectedCols<-read.csv(file = f)
