b1c60f82cf86f38d70a70d799b0f53f8d7543c4d
Jean Leão <JCLSoftware@users.noreply.github.com>
2018-10-22 17:14:24 -0300

    Update LoadPNAD.R

--- src/LoadPNAD.R
+++ src/LoadPNAD.R
@@ -1,3 +1,4 @@
+library(RCurl)
 #LEITURA DA PNAD 2018: 
 #make sure to set correctly your working directory Ex: setwd("~/Projetcs/R/PNADRegression/")
 # mostrar até 8 casas decimais options("scipen" = 8) 
