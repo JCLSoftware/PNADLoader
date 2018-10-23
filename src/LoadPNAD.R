41cb4e8c681b2bf933d154cd5225f417e4caab9d
Jean Leão <JCLSoftware@users.noreply.github.com>
2018-10-23 09:37:04 -0300

    Update LoadPNAD.R

--- src/LoadPNAD.R
+++ src/LoadPNAD.R
@@ -5,7 +5,11 @@
 #Arquivo de metadados (propriedades dos campos de dados)
 #sourceMeta<-'https://raw.githubusercontent.com/JCLSoftware/PNADRegression/master/data/meta.csv'
 #target<-paste(source, '.txt', sep='')
-target<-'meta.txt'
+sourceMeta<-'https://raw.githubusercontent.com/JCLSoftware/PNADRegression/master/data/meta.zip'
+selectedFields<-c("UF","V2007","V2009","V2010","VD3002","VD4020","V1023","V1022","V1028")
+source('https://raw.githubusercontent.com/JCLSoftware/PNADLoader/master/src/LoadPNAD.R?a=12333')
+##
+target<-'metaclear.txt'
 #Arquivo de dados
 #sourceData<-'data/PNADC_012017_20180816.zip'
 
@@ -21,18 +25,42 @@ readMeta<-function(src){
   writeLines(tx2, con=target)
   read.csv(file=target, sep = ' ', header = F)
 }
-selectFields<-function(srczip, selectedFields){
-      result <- read.fwf(file=unz(srczip, gsub(pattern = '.zip$', replace = '.txt', x = basename(srczip)), widths=config$V3))
+getTxtFromZiped<-function(url){
+  getFromZiped(url, '.txt')
+}
+getFromZiped<-function(url, ext){  
+  result<-gsub(pattern = '.zip$', replace = ext, x = (basename(url))) 
+  if(!file.exists(result)){
+    if(!file.exists(basename(url))){
+      tmp<-download.file(url, basename(url), method= "auto", cacheOK = TRUE)
+    }
+    #write(unz(basename(url), result), result)
+    unzip(basename(url))
+  }
+  return (result)
+}
+#downloadTxtZiped('https://raw.githubusercontent.com/JCLSoftware/PNADRegression/master/data/meta.zip')
+selectFields<-function(srczip, srcmetazip, selectedFields){
+      message("Carregando configurações...")
+      config<-readMeta(getTxtFromZiped(srcmetazip))
+      message("Carregando dados...")
+      result <- read.fwf(file=getTxtFromZiped(srczip), widths=config$V3)
       names(result) <-config$V2
       #View(pes2018)
       summary(result) 
-      selectedCols <- subset(result, select=selectedFields)
-      #write.csv(pes2018b, file=csvFile, row.names = F)
+      #selectedCols<-read.csv(file = getDataFile())
+      message("Selecionando dados...")
+      selectedCols <- subset(result, select=selectedFields)      
       #Zip file .csv -> .zip  
       #if(!file.exists(sourceData)){
       #  zip(zipfile = sourceData, files = csvFile)
       #}  
       return (result)
+}
+buildSelectFieldsCSV<-function(srczip, srcmetazip, selectedFields){
+  csvFile<-gsub(pattern = '.zip$', replace = '.csv', x = srczip)
+  write.csv(selectFields(srczip, srcmetazip, selectedFields), file=csvFile, row.names = F)
+  return (csvFile)
 }
 getDataFile<-function(){
     if(file.exists(sourceData)){
