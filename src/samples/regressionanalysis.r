aa66ba5d5461c2f26543232ba9b16d79b8d655f6
santosbrenda <44342597+santosbrenda@users.noreply.github.com>
2018-10-26 23:35:50 -0300

    added comments, fixes
    library car (install and load)
    comments

--- src/samples/regressionanalysis.r
+++ src/samples/regressionanalysis.r
@@ -1,7 +1,7 @@
-
-# Make sure to set correctly your working directory Ex: `setwd("~/data/")`.
+# Preparo do ambiente
 setwd("~/data/")
 
+# Carga e seleção dos dados
 source('https://raw.githubusercontent.com/JCLSoftware/PNADLoader/master/src/LoadPNAD.R')
 sourceDataSelected<-'https://raw.githubusercontent.com/JCLSoftware/PNADLoader/master/data/PNADC_012017_20180816i.zip'
 selectedCols<-readCSVZip(sourceDataSelected)
@@ -9,41 +9,49 @@ selectedCols<-readCSVZip(sourceDataSelected)
 #renaming the columns
 names(selectedCols)<-c("uf","sexo","idade","cor","anoestudo","renda","areacensitaria","situacaocensitaria","pesopes")
 
-selectedColsb <- subset(selectedCols, select=c("uf","sexo","idade","cor","anoestudo","renda","areacensitaria","situacaocensitaria","pesopes"))
-selectedColsc<-subset(selectedColsb, uf>=31 & uf<=35 )
+#Já foi feito no PNADLoader, não é necessário aqui: selectedColsb <- subset(selectedCols, select=c("uf","sexo","idade","cor","anoestudo","renda","areacensitaria","situacaocensitaria","pesopes"))
+selectedCols<-subset(selectedCols, uf>=31 & uf<=35 )
 
-rm(selectedCols,selectedColsb)
-summary(selectedColsc)
-selectedColsd <- subset(selectedColsc, renda!=999999999999 & renda!=0 & cor!=9 &anoestudo!= 17 )
-attach(selectedColsd)
-str(sexo)
-sexomasc<-numeric(62260)
-ehrural<-numeric(62260) 
-naometropole<- numeric(62260) 
-preta<- numeric(62260) 
-amarelo<- numeric(62260)
-parda<- numeric(62260) 
-indigena<- numeric(62260)
-for (cont in 1:62260) { 
-  if (selectedColsd$sexo[cont] ==2){ 
+summary(selectedCols)
+selectedCols <- subset(selectedCols, renda!=999999999999 & renda!=0 & cor!=9 &anoestudo!= 17 )
+attach(selectedCols)
+sexo<-str(sexo)
+l<-nrow(selectedCols)
+sexomasc<-numeric(l)
+ehrural<-numeric(l) 
+naometropole<- numeric(l) 
+preta<- numeric(l) 
+amarelo<- numeric(l)
+parda<- numeric(l) 
+indigena<- numeric(l)
+for (cont in 1:l) { 
+  if (selectedCols$sexo[cont] ==2){ 
     sexomasc[cont] <-1 }
   else { sexomasc[cont]<-0 } } 
-for (cont in 1:62260) { 
-  if (selectedColsd$situacaocensitaria[cont]>=1 & selectedColsd$situacaocensitaria[cont]<=3 ){ 
+for (cont in 1:l) { 
+  if (selectedCols$situacaocensitaria[cont]>=1 & selectedCols$situacaocensitaria[cont]<=3 ){ 
     ehrural[cont] <-0 }else { ehrural[cont]<-1 } } 
-for (cont in 1:62260) { 
-  if (selectedColsd$areacensitaria[cont]==1){ 
+for (cont in 1:l) { 
+  if (selectedCols$areacensitaria[cont]==1){ 
     naometropole[cont] <-0 }
   else { naometropole[cont]<-1 } }
-if (selectedColsd$cor[cont]==6){ 
+if (selectedCols$cor[cont]==6){ 
   amarelo[cont]<-1 }
-if (selectedColsd$cor[cont]==8){ 
+if (selectedCols$cor[cont]==8){ 
   parda[cont]<- 1 } 
-if (selectedColsd$cor[cont]==0){ indigena[cont]<- 1 } 
+if (selectedCols$cor[cont]==0){ indigena[cont]<- 1 } 
+
+#Regressão
+#Verifica se o pacote "car" está instalado e se não tiver efetua a instalação
+list.of.packages <- c("car")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+library("car")
 
 lrenda<-log(renda)
 summary(lrenda)
 modelo1<- lm(lrenda~uf+sexomasc+idade+preta+amarelo+parda+indigena+anoestudo+ehrural+naometropole,weights=pesopes)
+#Qual a melhor forma de apresentar o sumário no texto? Uma tabela?
 summary(modelo1)
 
 residualPlots(modelo1,type="rstudent")
